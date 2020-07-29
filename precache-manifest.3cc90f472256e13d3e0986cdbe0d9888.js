1eda99cf6eb40ca40f2c4203c5f3a15ad2240ac0
zinmar <zinmarmyolwin2018@gmail.com>
2020-07-29 11:01:19 +0630

    Updates

---
+++ precache-manifest.3cc90f472256e13d3e0986cdbe0d9888.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e014580da14885af664e1664b6f088",
+    "url": "/Zinmar9/TestReactjs/index.html"
+  },
+  {
+    "revision": "4014005b1796d94dc0bd",
+    "url": "/Zinmar9/TestReactjs/static/css/2.9481e47d.chunk.css"
+  },
+  {
+    "revision": "bb6ed1ea5b99cef128d4",
+    "url": "/Zinmar9/TestReactjs/static/css/main.c7df500f.chunk.css"
+  },
+  {
+    "revision": "4014005b1796d94dc0bd",
+    "url": "/Zinmar9/TestReactjs/static/js/2.baae9252.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Zinmar9/TestReactjs/static/js/2.baae9252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6ed1ea5b99cef128d4",
+    "url": "/Zinmar9/TestReactjs/static/js/main.32f7d9b2.chunk.js"
+  },
+  {
+    "revision": "e23ecb1b84e099aa6cf3",
+    "url": "/Zinmar9/TestReactjs/static/js/runtime-main.bab004f2.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/Zinmar9/TestReactjs/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
